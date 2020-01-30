8b5e08b87e2f0cce6c242288028ae1cb4a76b4b0
Rasheed <a.rasheedalabi@gmail.com>
2020-01-30 18:47:11 +0100

    production commit

--- README.md
+++
@@ -1,7 +0,0 @@
-
-                  The puzzle is won when all the lights are turned off. You can
-                  click on a cell to toggle that light -- but it also toggles
-                  the light above it, to the left of it, to the right of it and
-                  below it. (Cells on an edge or in the corner won't flip as
-                  many lights since they are missing some neighbors).
-
---
+++ precache-manifest.7e4da8ada34675b6b18999ad43007d35.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f897dccae52506ab3ae5e56c64fe265",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e8dc56cda18142ddf1e9",
+    "url": "/static/css/main.c628f1c0.chunk.css"
+  },
+  {
+    "revision": "1351885e5bc0a7009df1",
+    "url": "/static/js/2.bde84895.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.bde84895.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e8dc56cda18142ddf1e9",
+    "url": "/static/js/main.eab313d2.chunk.js"
+  },
+  {
+    "revision": "789f1627f74cf11ecd45",
+    "url": "/static/js/runtime-main.f2ca0c7d.js"
+  }
+]);
